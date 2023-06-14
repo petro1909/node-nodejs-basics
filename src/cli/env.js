8fa75dc2faaca5e98d22f7f802b9caab5b9db11e
petro1909 <petya-97_97@mail.ru>
2023-06-14 16:33:29 +0300

    feat: add implementation of parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+    const rssRegex = /^RSS_/;
+    let rssEnvArr = Array.from(Object.keys(process.env)).filter((item) => rssRegex.test(item));
+    rssEnvArr = rssEnvArr.map((item) => `${item}=${process.env[item]}`);
+    const rssEnvsStr = rssEnvArr.join("; ");
+    console.log(rssEnvsStr);
 };
 
 parseEnv();
