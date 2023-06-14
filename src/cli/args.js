87135e6f51efd6e8756862357d642f9c8188a82e
petro1909 <petya-97_97@mail.ru>
2023-06-14 16:21:02 +0300

    feat: add implementation of parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+    const argsArr = process.argv.slice(2);
+    const argsStrArray = [];
+    for(let i = 0, j = 0; i < argsArr.length; i += 2, j++) {
+      argsStrArray[j] = `${argsArr[i]} is ${argsArr[i + 1]}`;
+    }
+    const argsStr = argsStrArray.join(", ");
+    console.log(argsStr);
 };
 
 parseArgs();
