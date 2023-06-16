3cc07d5e19411047f2a8d52d007fe5ace7634e9f
petro1909 <petya-97_97@mail.ru>
2023-06-16 09:17:08 +0300

    feat: add implementation of performCalculations function

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,25 @@
+import path from 'path';
+import url from "url";
+import os from "os";
+import { Worker } from "worker_threads";
+
 const performCalculations = async () => {
-    // Write your code here
+  const currentDirectory = path.dirname(url.fileURLToPath(import.meta.url));
+  const processFile = path.join(currentDirectory, "worker.js");
+
+    const cpuCores = os.cpus().length;
+    const workers = [];
+    for(let i = 10; i < cpuCores + 10 ; i++) {
+      workers.push(new Promise((resolve) => {
+        const worker = new Worker(processFile, {workerData: {number: i}});
+        worker.on("error", (err) => resolve({status: 'error', data: null}));
+        worker.on("message", (value) => resolve({status: 'resolved', data: value}));
+      }));
+    }
+    const workerResults = await Promise.all(workers);
+    workerResults.forEach((element) => {
+      console.log(element);
+    });
 };
 
 await performCalculations();
