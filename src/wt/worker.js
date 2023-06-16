0dac32791757a6a8220863ad1d7875cfb6237181
petro1909 <petya-97_97@mail.ru>
2023-06-16 09:16:42 +0300

    feat: add implementation of sendResult function

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,11 @@
+import {parentPort, workerData} from "worker_threads";
 // n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+const nthFibonacci = (n) => { return n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2); }
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    const arg = workerData.number;
+    const result = nthFibonacci(arg);
+    parentPort.postMessage(result);
 };
 
 sendResult();
